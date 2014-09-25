38930bb4b315383328ae1d4786a6e3192eef9bee
Huang Hao <hao.h.huang@intel.com>
2014-09-26 00:04:43 +0800

    Add classifiers in setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,15 @@ setup(
     version=dmidecode.__version__,
     py_modules=["dmidecode"],
     license='MIT',
-    platforms='linux',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        ],
     )
